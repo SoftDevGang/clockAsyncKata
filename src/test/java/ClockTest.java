cad90da262dbe6db4d9ef15de219790a602845dc
Alexandru Bolboaca <alexboly@gmail.com>
2016-11-18 11:25:23 +0200

    When slow server then time out

--- src/test/java/ClockTest.java
+++ src/test/java/ClockTest.java
@@ -3,9 +3,12 @@ import org.junit.Test;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class ClockTest {
+
+	private boolean serverOverhelmed;
 
 	@Test
 	public void workingClockIn() throws ExecutionException, InterruptedException {
@@ -14,12 +17,30 @@ public class ClockTest {
 		assertTrue(success.get());
 	}
 
+	@Test
+	public void slowClockIn() throws ExecutionException, InterruptedException {
+		setServerOverhelmed(true);
+
+		CompletableFuture<Boolean> success = clockInAsync("Steve", "5:45");
+
+		assertFalse(success.get());
+	}
+
+
 	private CompletableFuture<Boolean> clockInAsync(String userName, String timestamp) {
 		return CompletableFuture.supplyAsync(() -> this.recordTime(userName, timestamp));
 	}
 
 	private boolean recordTime(String userName, String timestamp) {
-		return true;
+		boolean canRecordTime = !serverOverhelmed;
+		return canRecordTime;
 	}
 
+	public void setServerOverhelmed(boolean serverOverhelmed) {
+		this.serverOverhelmed = serverOverhelmed;
+	}
+
+	public boolean isServerOverhelmed() {
+		return serverOverhelmed;
+	}
 }
