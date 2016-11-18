ea3255ae0f8e932d8b69f6db33cee7c29f6e0fcb
Peter Kofler <peter.kofler@code-cop.org>
2016-11-18 15:35:33 +0100

    Add comments what to do next, renames and move methods around.

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -5,43 +5,50 @@ import java.util.concurrent.TimeoutException;
 
 public class Client {
 
-	private Server server;
+    private final Server server;
 
-	public Client(Server server) {
-		this.server = server;
-	}
+    public Client(Server server) {
+        this.server = server;
+    }
 
-	public CompletableFuture<String> clockInAsync(String userName, String timestamp) {
-		return CompletableFuture.supplyAsync(
-				() -> server.recordTime(userName, timestamp)
-		);
-	}
+    public String clockIn(String userName, String timestamp) {
+        CompletableFuture<String> success = clockInAsync(userName, timestamp);
 
-	public String clockIn(String userName, String timestamp) {
-		CompletableFuture<String> success = clockInAsync(userName, timestamp);
-		try {
-			return success.get(10, TimeUnit.MILLISECONDS);
-		} catch (TimeoutException |
-				InterruptedException |
-				ExecutionException e) {
-			success.cancel(true);
-			try {
-				success.get();
-			} catch (InterruptedException e1) {
-				e1.printStackTrace();
-			} catch (ExecutionException e1) {
-				e1.printStackTrace();
-			}
-			System.out.println("Canceled");
-			return "Failure";
-		}
-	}
+        // TODO we want to timeout a non responding server. 1st try - get with timeout
+        // Put this into original future.
+        try {
+            return success.get(10, TimeUnit.MILLISECONDS);
+        } catch (TimeoutException |
+                InterruptedException |
+                ExecutionException e) {
 
-	public CompletableFuture<String> clockInWithTimeout(String userName, String timestamp) {
-		return CompletableFuture.supplyAsync(() -> this.clockIn(userName, timestamp));
+            // TODO we want to cancel the while loop on running future. 2nd try - cancel and get
+            System.out.println("Canceled");
+            success.cancel(true);
 
-		// 1. What we'd like to have is setting timeout on the method like this:
-		// return clockInAsync(userName, timestamp).setTimeout(10, "Failure");
-		// 2. What do we need to implement to cancel the CompletableFuture?
-	}
+            try {
+                success.get();
+            } catch (InterruptedException | ExecutionException ex) {
+                e.printStackTrace();
+            }
+
+            return "Failure";
+        }
+    }
+
+    private CompletableFuture<String> clockInAsync(String userName, String timestamp) {
+        // making it async seems too easy - just wrap future around it?
+        return CompletableFuture.supplyAsync(
+                () -> server.recordTime(userName, timestamp)
+        );
+    }
+
+    public CompletableFuture<String> clockInAsyncWithTimeout(String userName, String timestamp) {
+        // TODO make the timeout into future
+        return CompletableFuture.supplyAsync(() -> this.clockIn(userName, timestamp));
+
+        // 1. What we'd like to have is setting timeout on the method like this:
+        // e.g. return clockInAsync(userName, timestamp).setTimeout(10, "Failure");
+        // 2. What do we need to implement to cancel the CompletableFuture?
+    }
 }
--- src/test/java/ClockTest.java
+++ src/test/java/ClockTest.java
@@ -1,3 +1,4 @@
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.concurrent.CompletableFuture;
@@ -8,86 +9,75 @@ import static org.junit.Assert.assertEquals;
 
 public class ClockTest {
 
-	@Test
-	public void workingClockIn() throws ExecutionException, InterruptedException {
-		Client client = new Client((__, ___) -> "OK");
+    @Test
+    public void syncServerAccepts() throws ExecutionException, InterruptedException {
+        Client client = new Client((__, ___) -> "OK");
 
-		String isSuccessful = client.clockIn("Steve", "5:45");
+        String isSuccessful = client.clockIn("Steve", "5:45");
 
-		assertEquals("OK", isSuccessful);
-	}
+        assertEquals("OK", isSuccessful);
+    }
 
-	@Test
-	public void slowClockIn() throws ExecutionException, InterruptedException, TimeoutException {
-		Client client = new Client((__, ___) -> "Failure");
+    @Test
+    public void syncServerRejects() throws ExecutionException, InterruptedException, TimeoutException {
+        Client client = new Client((__, ___) -> "Failure");
 
-		String isSuccessful = client.clockIn("Steve", "5:45");
+        String isSuccessful = client.clockIn("Steve", "5:45");
 
-		assertEquals("Failure", isSuccessful);
-	}
+        assertEquals("Failure", isSuccessful);
+    }
 
-	@Test
-	public void timeOutClockIn() throws ExecutionException, InterruptedException, TimeoutException {
-		Client client = new Client((__, ___) -> {
-			while (true) {
-			}
-		});
+    @Test
+    public void syncServerThrowsException() throws ExecutionException, InterruptedException, TimeoutException {
+        Client client = new Client((__, ___) -> {
+            throw new IllegalArgumentException("server crashed");
+        });
 
-		String isSuccessful = client.clockIn("Steve", "5:45");
+        String isSuccessful = client.clockIn("Steve", "5:45");
 
-		assertEquals("Failure", isSuccessful);
-	}
+        assertEquals("Failure", isSuccessful);
+    }
 
-	@Test
-	public void timeOutAsyncWrapper() throws ExecutionException, InterruptedException, TimeoutException {
-		Client client = new Client((__, ___) -> {
-			while (true) {
-			}
-		});
+    @Test
+    public void syncServerDoesNotAnswer() throws ExecutionException, InterruptedException, TimeoutException {
+        Client client = new Client((__, ___) -> {
+            while (true) {
+            }
+        });
 
-		CompletableFuture<String> successful = client.clockInWithTimeout("Steve", "5:45");
+        String isSuccessful = client.clockIn("Steve", "5:45");
 
-		assertEquals("Failure", successful.get());
-	}
+        assertEquals("Failure", isSuccessful);
+    }
 
-	@Test
-	public void noBlocking() throws ExecutionException, InterruptedException, TimeoutException {
-		Client client = new Client((__, ___) -> {
-			while (true) {
-				try {
-					Thread.sleep(500);
-				} catch (InterruptedException e) {
-					System.out.println("Call: Interrupted");
-				}
-				System.out.println(".");
-			}
-		});
+    @Test @Ignore
+    public void timeOutAsyncWrapperNoBlocking() throws ExecutionException, InterruptedException, TimeoutException {
+        Client client = new Client((__, ___) -> {
+            while (true) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    System.out.println("Call: Interrupted");
+                }
+                System.out.println(".");
+            }
+        });
 
-		CompletableFuture<String> successful = client.clockInWithTimeout("Steve", "5:45");
+        CompletableFuture<String> successful = client.clockInAsyncWithTimeout("Steve", "5:45");
 
-		System.out.println("Doing more stuff");
+        System.out.println("Doing more stuff");
 
-		successful.whenComplete((s, throwable) -> {
-			try {
-				System.out.println("Done");
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		});
+        // do not check with GET, check on complete?
+        //        successful.whenComplete((s, throwable) -> {
+        //            try {
+        //                System.out.println("Done");
+        //            } catch (Exception e) {
+        //                e.printStackTrace();
+        //            }
+        //        });
 
-		Thread.sleep(10000);
+        Thread.sleep(10000);
 
-		assertEquals("Failure", successful.get());
-	}
-
-	@Test
-	public void exceptionInClockIn() throws ExecutionException, InterruptedException, TimeoutException {
-		Client client = new Client((__, ___) -> {
-			throw new Error();
-		});
-
-		String isSuccessful = client.clockIn("Steve", "5:45");
-
-		assertEquals("Failure", isSuccessful);
-	}
+        assertEquals("Failure", successful.get());
+    }
 }
